2b447f8357e6f39a599d3c213595a9641721a723
Michael Chen <mchen@drchenmath.com>
2019-03-02 08:28:21 -0500

    add favicon, and other new files

---
+++ imports/api/tasks.test.js
@@ -0,0 +1,15 @@
+/* eslint-env mocha */
+ 
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
+import { Tasks } from './tasks.js';
+ 
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+      it('can delete owned task', () => {
+      });
+    });
+  });
+}
